038e6289ad265a5d14e03220d4aba2a1ad875f25
mckasapbasi <mckasap@gmail.com>
2023-03-08 12:03:51 +0300

    ikinci komit

--- app/src/main/java/com/example/week3_2023_03/MainActivity.java
+++ app/src/main/java/com/example/week3_2023_03/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.week3_2023_03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setListAdapter( new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.country)));
     }
 }
